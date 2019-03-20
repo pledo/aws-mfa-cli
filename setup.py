02ae0b6e8fea496b46452b2ba3c565717514d472
Paulo Ledo <paulo.ledo@elinvar.de>
2019-03-20 23:19:53 +0100

    Adding the packing component, setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aws-mfa-cli',
+    version='0.1.0',
+    description='Easily configure the temporary credentials',
+    long_description='Get temporary credentials and put it on ~/.aws/credentials',
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='Paulo Ledo',
+    author_email='paulo.ledo@gmail.com',
+    packages=find_packages(),
+    package_data={'awsmfa': ['default_template/credentials', 'templates/credentials']},
+    scripts=['aws-mfa-cli'],
+    entry_points={
+        'console_scripts': [
+            'aws-mfa-cli=awsmfa:main',
+        ],
+    },
+    url='https://github.com/pledo/aws-mfa-cli',
+    install_requires=['boto3', 'jinja2'],
+    include_package_data=True
+
+)
